1afcefa42a37b9bd47a8db13795c9864860f5f3b
Tiago M Palte <tiagompalte@hotmail.com>
2017-10-19 01:20:16 -0200

    Adicionado métodos ao usuário controle

--- src/main/java/br/com/utfpr/porta/controle/UsuarioControle.java
+++ src/main/java/br/com/utfpr/porta/controle/UsuarioControle.java
@@ -3,13 +3,17 @@ package br.com.utfpr.porta.controle;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+import javax.validation.Valid;
+
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +23,7 @@ import br.com.utfpr.porta.modelo.Usuario;
 import br.com.utfpr.porta.repositorio.Portas;
 import br.com.utfpr.porta.repositorio.Usuarios;
 import br.com.utfpr.porta.response.Response;
+import br.com.utfpr.porta.seguranca.dto.AutenticacaoSenhaDto;
 import br.com.utfpr.porta.seguranca.dto.TokenDto;
 import br.com.utfpr.porta.servico.AutorizacaoServico;
 import br.com.utfpr.porta.servico.LogServico;
@@ -38,10 +43,10 @@ public class UsuarioControle {
 	
 	@Autowired
 	private Usuarios usuariosRepositorio;
-	
-	@RequestMapping(value="/rfid/{rfid}/{codigo_porta}", method=RequestMethod.GET)
+		
+	@RequestMapping(value="/rfid/{rfid}", method=RequestMethod.GET)
 	public ResponseEntity<?> obterUsuarioPorRFID(@RequestHeader(value="zone") String zone,
-			@PathVariable String rfid, @PathVariable Long codigo_porta) {
+			@RequestHeader(value="porta") Long codigo_porta, @PathVariable String rfid) {
 		
 		Response<TokenDto> response = new Response<TokenDto>();
 		
@@ -55,10 +60,6 @@ public class UsuarioControle {
 		
 		if(Strings.isEmpty(zone)) {
 			response.addError("Zona local não informada");
-		}
-		
-		if(response.getErrors() != null && !response.getErrors().isEmpty()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);			
 		}
 		
 		LocalDateTime dataHora;		
@@ -83,10 +84,84 @@ public class UsuarioControle {
 			response.addError("Usuário sem autorização para acesso a porta desejada");
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
 		}
+				
+		return ResponseEntity.status(HttpStatus.OK).body(usuario);			
+	}
+	
+	@RequestMapping(value="/autenticacaoSenha", method=RequestMethod.POST)
+	public ResponseEntity<?> autenticacaoPorSenhaDigitada(@RequestHeader(value="zone") String zone,
+			@RequestHeader(value="porta") Long codigo_porta,
+			@Valid @RequestBody AutenticacaoSenhaDto autenticacaoSenha) {
+		
+		Response<TokenDto> response = new Response<TokenDto>();
+				
+		if(codigo_porta == null) {
+			response.addError("Código da porta não informado");
+		}
+		
+		if(Strings.isEmpty(zone)) {
+			response.addError("Zona local não informada");
+		}
+		
+		if(response.getErrors() != null && !response.getErrors().isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);			
+		}
+		
+		LocalDateTime dataHora;		
+		try {
+			dataHora = LocalDateTime.now(ZoneId.of(zone));
+		}
+		catch(Exception e) {
+			response.addError("Zona local formatada incorretamente");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		}
+		
+		Usuario usuario = usuariosRepositorio.findByRfid(autenticacaoSenha.getRfid());
+		
+		Porta porta = portasRepositorio.findOne(codigo_porta);
+		
+		if(usuario == null || porta == null) {
+			response.addError("Usuário e/ou porta não encontrado");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+		}
+		
+		if(!autorizacaoServico.validarAcessoUsuario(porta, usuario, dataHora)) {
+			response.addError("Usuário sem autorização para acesso a porta desejada");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+		}
+		
+		if(!BCrypt.checkpw(autenticacaoSenha.getSenha(), usuario.getSenha())) {
+			response.addError("Senha incorreta");
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+		}
+						
+		logServico.entrarPorta(usuario, porta);
+		
+		response.setData(new TokenDto("Autorizado"));
+				
+		return ResponseEntity.ok(response);			
+	}
+	
+	@RequestMapping(value="/confirmacaoAcesso/{rfid}", method=RequestMethod.POST)
+	public ResponseEntity<?> confirmacaoAcesso(@RequestHeader(value="porta") Long codigo_porta, @PathVariable String rfid) {
+		
+		Response<TokenDto> response = new Response<TokenDto>();
+		
+		Usuario usuario = usuariosRepositorio.findByRfid(rfid);
+		
+		Porta porta = portasRepositorio.findOne(codigo_porta);
+		
+		if(usuario == null || porta == null) {
+			response.addError("Usuário e/ou porta não encontrado");
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+		}
 		
 		logServico.entrarPorta(usuario, porta);
 		
-		return ResponseEntity.status(HttpStatus.OK).body(usuario);			
+		response.setData(new TokenDto("Log registrado"));
+		
+		return ResponseEntity.ok(response);
 	}
+	
 
 }
