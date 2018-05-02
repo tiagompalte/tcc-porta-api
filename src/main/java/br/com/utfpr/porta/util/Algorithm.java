2fe35c0eb153a3f794ba0ce69d92dbdef13df095
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-05-02 18:30:31 -0300

    Algoritmo com número de amostras em 22k

--- src/main/java/br/com/utfpr/porta/util/Algorithm.java
+++ src/main/java/br/com/utfpr/porta/util/Algorithm.java
@@ -1,18 +1,19 @@
 package br.com.utfpr.porta.util;
 
 public class Algorithm {
-	
-	private static final int DELAY_MAX = 1600;	
-	
+
+	private static final int NUM_AMOSTRAS = 22000; // 1.5*16000
+	private static final int DELAY_MAX = 1600;
+
 	private Algorithm() {
 		throw new IllegalStateException("Utility class");
 	}
-	
+
 	private static float max(int nElem, float[] buffer) {
-		
+
 		float maior = 0;
 
-		for (int i=0; i<nElem; i++) {
+		for (int i = 0; i < nElem; i++) {
 			if (buffer[i] > maior) {
 				maior = buffer[i];
 			}
@@ -20,61 +21,47 @@ public class Algorithm {
 
 		return maior;
 	}
-	
-	private static int[] zeroFill (int[] buffer) {
-		int[] bufferZ = new int[buffer.length + 2*DELAY_MAX];
-		
-		for (int i=DELAY_MAX; i<buffer.length + DELAY_MAX; i++) {
+
+	private static int[] zeroFill(int[] buffer) {
+		int[] bufferZ = new int[NUM_AMOSTRAS + 2 * DELAY_MAX];
+
+		for (int i = DELAY_MAX; i < NUM_AMOSTRAS + DELAY_MAX; i++) {
 			bufferZ[i] = buffer[i - DELAY_MAX];
 		}
-		
+
 		return bufferZ;
 	}
-	
+
 	private static float autoCorr(int[] buffer) {
-		
+
 		float rAuto = 0;
 
-		for (int n = 0; n < (buffer.length + 2*DELAY_MAX) - 1; n++) {
-			rAuto += (buffer[n]*buffer[n])/255;
+		for (int n = 0; n < (NUM_AMOSTRAS + 2 * DELAY_MAX) - 1; n++) {
+			rAuto += (buffer[n] * buffer[n]) / 255;
 		}
-		
+
 		return rAuto;
 	}
 
 	private static float crossCorr(int[] bufferDatabase, int[] bufferRecebido) {
-		
-		float[] rCross = new float[2*DELAY_MAX + 1];
-		
-		if (bufferDatabase.length <= bufferRecebido.length) {
-			for (int k = -DELAY_MAX; k <= DELAY_MAX; k++) {
-				if (k <= 0) {
-					for (int n = 0; n < (bufferDatabase.length + 2*DELAY_MAX + k); n++) {
-						rCross[DELAY_MAX + k] += (bufferDatabase[n]*bufferRecebido[n-k])/255;
-					}
-				} else {
-					for (int n = 0; n < (bufferDatabase.length + 2*DELAY_MAX - k); n++) {
-						rCross[DELAY_MAX + k] += (bufferDatabase[n+k]*bufferRecebido[n])/255;
-					}
+
+		float[] rCross = new float[2 * DELAY_MAX + 1];
+
+		for (int k = -DELAY_MAX; k <= DELAY_MAX; k++) {
+			if (k <= 0) {
+				for (int n = 0; n < (NUM_AMOSTRAS + 2 * DELAY_MAX + k); n++) {
+					rCross[DELAY_MAX + k] += (bufferDatabase[n] * bufferRecebido[n - k]) / 255;
 				}
-			}
-		} else {
-			for (int k = -DELAY_MAX; k <= DELAY_MAX; k++) {
-				if (k <= 0) {
-					for (int n = 0; n < (bufferRecebido.length + 2*DELAY_MAX + k); n++) {
-						rCross[DELAY_MAX + k] += (bufferDatabase[n]*bufferRecebido[n-k])/255;
-					}
-				} else {
-					for (int n = 0; n < (bufferRecebido.length + 2*DELAY_MAX - k); n++) {
-						rCross[DELAY_MAX + k] += (bufferDatabase[n+k]*bufferRecebido[n])/255;
-					}
+			} else {
+				for (int n = 0; n < (NUM_AMOSTRAS + 2 * DELAY_MAX - k); n++) {
+					rCross[DELAY_MAX + k] += (bufferDatabase[n + k] * bufferRecebido[n]) / 255;
 				}
 			}
 		}
 
-		return max(2*DELAY_MAX + 1, rCross);
+		return max(2 * DELAY_MAX + 1, rCross);
 	}
-		
+
 	public static boolean validate(double tolerancia, int[] bufferDatabase, int[] bufferRecebido) throws Exception {
 		
 		try {			
@@ -85,7 +72,7 @@ public class Algorithm {
 			float auto2 = autoCorr(bufferRecebidoZ);
 			float cross12 = crossCorr(bufferDatabaseZ, bufferRecebidoZ);
 			
-			double coef = cross12 / Math.sqrt(auto1*auto2);
+			double coef = cross12 / Math.sqrt(auto1 * auto2);
 			
 			return coef >= (1 - tolerancia);
 		}
@@ -95,4 +82,3 @@ public class Algorithm {
 	}
 
 }
-
